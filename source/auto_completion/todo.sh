5ecf0504ff4baa8b73f7979a7b33b6e7e0ca0a19
Valentin <dprosium@gmail.com>
2016-06-11 17:08:54 +0200

    Auto completion for contexts on a bare todo and auto completion for visibility values

--- source/auto_completion/todo.sh
+++ source/auto_completion/todo.sh
@@ -5,22 +5,30 @@ _todo()
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
     commands="add done task edit rm ctx contexts history purge"
+
+    data_dir="$HOME/.toduh"
+    if [ -d '.toduh' ]; then
+        data_dir='.toduh'
+    fi
+    data_file="$data_dir/contexts"
+    if [ -f $data_file ]; then
+        contexts=`cat $data_file`
+    fi
+
     if [ ${prev} = 'todo' ]; then
-        COMPREPLY=( $(compgen -W "${commands}" ${cur}) )
+        COMPREPLY=( $(compgen -W "${commands} ${contexts} LAST" ${cur}) )
         return 0
     fi
 
-    if [ ${prev} = '-c' -o ${prev} = 'ctx' ]; then
-        data_dir="$HOME/.toduh"
-        if [ -d '.toduh' ]; then
-            data_dir='.toduh'
-        fi
-        data_file="$data_dir/contexts"
-        if [ -f $data_file ]; then
-            contexts=`cat $data_file`
-            COMPREPLY=( $(compgen -W "${contexts}" ${cur}) )
-            return 0
-        fi
-    fi       
+    if [ ${prev} = '-c' -o ${prev} = '--context' -o ${prev} = 'ctx' ]; then
+        COMPREPLY=( $(compgen -W "${contexts} LAST" ${cur}) )
+        return 0
+    fi
+
+    if [ ${prev} = '-v' -o ${prev} = '--visibility' ]; then
+        visibilities="hidden discreet wide"
+        COMPREPLY=( $(compgen -W "${visibilities}" ${cur}) )
+        return 0
+    fi
 }
 complete -F _todo todo
