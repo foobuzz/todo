7d720790fba4b0cca817e7dd17a3a2b04b1bad48
Valentin <dprosium@gmail.com>
2016-07-05 21:16:52 +0200

    Got rid of the standard palette

--- source/todo/rainbow.py
+++ source/todo/rainbow.py
@@ -1,10 +1,9 @@
-import re
-
-
 """ This module deals with support of the color in the terminal through ANSI
 escape codes. It defines the high level class ColoredStr which is a string
 containing color escape codes ready to be printed in color to the terminal.
 """
+
+import re
 
 
 # There are three possible types of ANSI escape codes to print text in colors:
@@ -27,28 +26,11 @@ containing color escape codes ready to be printed in color to the terminal.
 # supported. I prefer to let the user chose what he wants to use via a
 # configuration file. Using an unsupported escape code can result in no color
 # at all, a wrong color, or the escape code being printed literally.
-# I designate each of these codes respectively by 3, 8 and 24 which is the
-# number bits necessary to represent their integer code(s).
-
 
 ANSI_TEMPLATES = {
-	3:  '\33[3{}m',          # Basic 8 colors
-	8:  '\33[38;5;{}m',      # 256 colors
-	24: '\33[38;2;{};{};{}m' # 24-bit RGB true colors
-}
-
-# Those are the user-friendly names of the "palettes" supported by the module.
-# A palette corresponds to one of the possible ANSI escape code. Two palettes
-# map to the same escape code. That's the escape code for 256 colors, which
-# itself should be interpreted according to a 256-color palette.
-# The palette named "256" uses 3 bits for red, 3 bits for green and 2 bits for
-# blue, storing the integer value as a RRRGGGBB byte.
-# The palette named "xterm-256" uses the specific x-term palette.
-PALETTES = {
-	'8': 3,
-	'256': 8,
-	'xterm-256': 8,
-	'rgb': 24
+	'8':         '\33[3{}m',          # Basic 8 colors
+	'xterm-256': '\33[38;5;{}m',      # 256 colors
+	'rgb':       '\33[38;2;{};{};{}m' # 24-bit RGB true colors
 }
 
 # ANSI escape codes are switches, turning on a given behaviour for all the
@@ -74,13 +56,13 @@ BASIC_COLORS = [
 # Here I use 255 for convenience with the rest of the program.
 BASIC_RGB = [
 	(0, 0, 0),
-	(255, 0, 0),
-	(0, 255, 0),
-	(255, 255, 0),
-	(0, 0, 255),
-	(255, 0, 255),
-	(0, 255, 255),
-	(255, 255, 255)
+	(205, 0, 0),
+	(0, 205, 0),
+	(205, 205, 0),
+	(0, 0, 238),
+	(205, 0, 205),
+	(0, 205, 205),
+	(229, 229, 229)
 ]
 
 # Python's re doesn't support repeated capture :s
@@ -154,20 +136,18 @@ def get_color_values(color, palette):
 
 		palette: one of the keys of `PALETTES`
 	"""
-	level = PALETTES[palette]
 	rgb = None
 	if color in BASIC_COLORS:
 		index = BASIC_COLORS.index(color)
-		if level == 3:
+		if palette == '8':
 			return (index,)
 		else:
-			rgb = BASIC_RGB[index]
+			rgb = tuple(255 if c > 0 else 0 for c in BASIC_RGB[index])
 	elif is_in_palette(color):
-		if level == 8:
-			return (color,)
+		if palette == 'xterm-256':
+			return (int(color),)
 		else:
-			converter = get_to_rgb_converter(palette)
-			rgb = converter(color)
+			rgb = xterm_palette_to_rgb(color)
 	else:
 		rgb_match = re.match(RGB_REGEX, color)
 		if rgb_match is not None:
@@ -178,15 +158,15 @@ def get_color_values(color, palette):
 			if hexa_match is not None:
 				rgb = hexa_match.groups()
 				rgb = tuple(int(c, 16) for c in rgb)
-		if (rgb_match is not None or hexa_match is not None) and level == 24:
+		if (rgb_match is not None or hexa_match is not None) \
+		and palette == 'rgb':
 			return rgb
 	if rgb is not None:
-		if level == 3:
+		if palette == '8':
 			return (rgb_to_basic(rgb),)
-		elif level == 8:
-			converter = get_from_rgb_converter(palette)
-			return (converter(rgb),)
-		elif level == 24:
+		elif palette == 'xterm-256':
+			return (rgb_to_xterm_palette(rgb),)
+		elif palette == 'rgb':
 			return rgb
 
 
@@ -198,48 +178,23 @@ def is_in_palette(color):
 	return 0 <= value <= 255
 
 
-def get_to_rgb_converter(palette):
-	if palette == '256':
-		return standard_palette_to_rgb
-	elif palette == 'xterm-256':
-		return xterm_palette_to_rgb
-
-
-def get_from_rgb_converter(palette):
-	if palette == '256':
-		return rgb_to_standard_palette
-	elif palette == 'xterm-256':
-		return rgb_to_xterm_palette
-
-
 def rgb_to_basic(rgb):
-	bits = tuple(255 if c >= 128 else 0 for c in rgb)
-	return BASIC_RGB.index(bits)
-
-
-def standard_palette_to_rgb(color):
-	bits = bin(int(color))[2:].zfill(8)
-	rgb = bits[0:3], bits[4:7], bits[7:]
-	return tuple(int(int(s, 2) * 255 / (2**len(s)-1)) for s in rgb)
-
-
-def rgb_to_standard_palette(rgb):
-	bits = ''
-	for i, j in zip(rgb, [3, 3, 2]):
-		bits += bin(int(i * (2**j-1) / 255))[2:].zfill(j)
-	return int(bits, 2)
+	bits = tuple(1 if c >= 128 else 0 for c in rgb)
+	for i, color in enumerate(BASIC_RGB):
+		if bits == tuple(1 if c > 0 else 0 for c in color):
+			return i
 
 
 def xterm_palette_to_rgb(color):
 	color = int(color)
 	# Basic colors
 	if color <= 7:
-		return tuple(c - 128 for c in BASIC_RGB[color])
+		return BASIC_RGB[color]
 	if 8 <= color <= 15:
-		return BASIC_RGB[color-8]
+		return tuple(255 if c > 0 else 0 for c in BASIC_RGB[color - 8])
 	# Gray levels
 	if XTERM_GRAY_LEVELS_OFFSET <= color <= 255:
-		return (8 * (color - XTERM_GRAY_LEVELS_OFFSET + 1),) * 3
+		return (8 + 10*(color - XTERM_GRAY_LEVELS_OFFSET),) * 3
 	# Other colors
 	value = color - XTERM_COLORS_OFFSET
 	mod = value
@@ -287,8 +242,7 @@ class ColoredStr(str):
 		if color == DEFAULT:
 			return string
 		values = get_color_values(color.lower(), palette)
-		level = PALETTES[palette]
-		ansi_seq = ANSI_TEMPLATES[level].format(*values)
+		ansi_seq = ANSI_TEMPLATES[palette].format(*values)
 		literal = ansi_seq + string + ANSI_RESET
 		the_string = super().__new__(cls, literal)
 		setattr(the_string, 'length', len(string))
