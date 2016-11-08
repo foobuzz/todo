4aeb3838361c0c19e6a234766c1b24b5553577d3
Valentin <dprosium@gmail.com>
2016-11-08 14:00:35 +0100

    Updated bash completion

--- source/bash_completion/toduh.sh
+++ source/bash_completion/toduh.sh
@@ -4,7 +4,7 @@ _todo()
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
     prev="${COMP_WORDS[COMP_CWORD-1]}"
-    commands="add done task edit rm ctx contexts history purge --location --help"
+    commands="add done task edit rm ctx mv rmctx contexts history purge --location --help"
 
     data_dir="$HOME/.toduh"
     if [ -d '.toduh' ]; then
@@ -16,17 +16,22 @@ _todo()
     fi
 
     if [ ${prev} = 'todo' ]; then
-        COMPREPLY=( $(compgen -W "${commands} ${contexts} LAST" -- ${cur}) )
+        COMPREPLY=( $(compgen -W "${commands} ${contexts}" -- ${cur}) )
         return 0
     fi
 
-    if [ ${prev} = '-c' -o ${prev} = '--context' -o ${prev} = 'ctx' ]; then
-        COMPREPLY=( $(compgen -W "${contexts} LAST" ${cur}) )
+    if [[ ${prev} = '-c' ||
+          ${prev} = '--context' ||
+          ${prev} = 'ctx' ||
+          ${prev} = 'rmctx' ||
+          ${prev} = 'mv' ||
+          ${COMP_WORDS[COMP_CWORD-2]} = 'mv' ]]; then
+        COMPREPLY=( $(compgen -W "${contexts}" ${cur}) )
         return 0
     fi
 
     if [ ${prev} = '-v' -o ${prev} = '--visibility' ]; then
-        visibilities="hidden discreet wide"
+        visibilities="hidden normal"
         COMPREPLY=( $(compgen -W "${visibilities}" ${cur}) )
         return 0
     fi
